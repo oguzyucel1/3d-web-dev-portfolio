67cacafc91a6c9b45009146ccba7a6cfcb2b451e
Oğuz Yücel <oguzyucell.oy@gmail.com>
2025-06-11 04:05:42 +0300

    3d hero model added

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,53 +1,83 @@
-import React from 'react'
-import {words} from '../constants/index.js'
-import Button from '../components/Button.jsx'
-
+import React from "react";
+import { words } from "../constants/index.js";
+import Button from "../components/Button.jsx";
+import HeroExperience from "../components/HeroModels/HeroExperience.jsx";
+import { useGSAP } from "@gsap/react";
+import { gsap } from "gsap";
 
 const Hero = () => {
+  useGSAP(() => {
+    gsap.fromTo(
+      ".hero-text h1",
+      {
+        y: 50,
+        opacity: 0,
+      },
+      {
+        y: 0,
+        opacity: 1,
+        stagger: 0.4,
+        duration: 1.4,
+        ease: "power2.out",
+      }
+    );
+  });
+
   return (
     <section id="hero" className="relative overflow-hidden">
       <div className="absolute top-0 left-0 z-10">
-       <img src="/images/bg.png" alt="background" />
+        <img src="/images/bg.png" alt="background" />
       </div>
 
-      <div className='hero-layout'>
-   {/* LEFT SIDE : HERO CONTENT */}
-   <header className='flex flex-col justify-center md:w-full w-screen md:px-20 px-5'>
-    <div className="flex flex-col gap-7">
-      <div className="hero-text">
-        <h1>Shaping
-          <span className='slide'>
-            <span className='wrapper'>
-              {words.map((word) => (
-                <span key={word.text} className='flex items-center md:gap-3 gap-1 pb-2'  > 
-                  <img src={word.imgPath}
-                   alt={word.text}
-                   className='xl:size-12 md:size-10 size-7 md:p-2 p-1 rounded-full bg-white-50'
-                  />
-                  <span>{word.text}</span>
+      <div className="hero-layout">
+        {/* LEFT SIDE : HERO CONTENT */}
+        <header className="flex flex-col justify-center md:w-full w-screen md:px-20 px-5">
+          <div className="flex flex-col gap-7">
+            <div className="hero-text">
+              <h1>
+                Shaping
+                <span className="slide">
+                  <span className="wrapper">
+                    {words.map((word) => (
+                      <span
+                        key={word.text}
+                        className="flex items-center md:gap-3 gap-1 pb-2"
+                      >
+                        <img
+                          src={word.imgPath}
+                          alt={word.text}
+                          className="xl:size-12 md:size-10 size-7 md:p-2 p-1 rounded-full bg-white-50"
+                        />
+                        <span>{word.text}</span>
+                      </span>
+                    ))}
+                  </span>
                 </span>
-                ))}
-            </span> 
-          </span>
-        </h1>
-        <h1>into Real Projects</h1>
-        <h1>that Deliver Results</h1>
-      </div>
+              </h1>
+              <h1>into Real Projects</h1>
+              <h1>that Deliver Results</h1>
+            </div>
 
-      <p className="text-white-50 md:text-xl relative z-10 pointer-events-none">
-        Hi, I'm Oğuz, a developer based in Turkey with a passion for code.
-      </p>
-      <Button
-      className="md:w-80 md:h-16 w-60 h-12"
-      id="button"
-      text="See my Work" />
-    </div>
-   </header>
+            <p className="text-white-50 md:text-xl relative z-10 pointer-events-none">
+              Hi, I'm Oğuz, a developer based in Turkey with a passion for code.
+            </p>
+            <Button
+              className="md:w-80 md:h-16 w-60 h-12"
+              id="button"
+              text="See my Work"
+            />
+          </div>
+        </header>
 
-   {/* RIGHT SIDE : 3D MODEL */}
+        {/* RIGHT SIDE : 3D MODEL */}
+        <figure>
+          <div className="hero-3d-layout">
+            <HeroExperience />
+          </div>
+        </figure>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
