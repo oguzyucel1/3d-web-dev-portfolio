841f82c707a220650626f495ad9979ab44d1d7b9
Oğuz Yücel <oguzyucell.oy@gmail.com>
2025-06-27 13:33:44 +0300

    bug fixes

--- src/components/AnimatedCounter.jsx
+++ src/components/AnimatedCounter.jsx
@@ -6,15 +6,15 @@ const AnimatedCounter = () => {
   return (
     <div id="counter" className="padding-x-lg xl:mt-0 mt-32">
       <div className="mx-auto grid-4-cols">
-        {counterItems.map((item) => (
-          <div className="bg-zinc-900 rounded-lg p-10 flex flex-col  justify-center">
-            <div
-              key={counterItems.label}
-              className="counter-number text-white text-5xl font-bold mb-2"
-            >
+        {counterItems.map((item, index) => (
+          <div
+            key={item.label || index} // ✅ key burada olmalı
+            className="bg-zinc-900 rounded-lg p-10 flex flex-col justify-center"
+          >
+            <div className="counter-number text-white text-5xl font-bold mb-2">
               <CountUp suffix={item.suffix} end={item.value} />
             </div>
-            <div className="text-white-50 text-lg"> {item.label}</div>
+            <div className="text-white-50 text-lg">{item.label}</div>
           </div>
         ))}
       </div>
